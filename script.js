a333327c8fb0e06f53f26ad2eb558a557830dc75
erykos111 <erykos111@gmail.com>
2022-04-23 15:46:38 +0100

    Add grid with basic colour change and reset button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function create_grid(){
     //Find the main div
     const main_div = document.querySelector('#main')
 
-    //Repeat 16 times
+    //Repeat 16 times to create 16 x 16 grid
     for(let i = 0; i < 16; i++){
 
         //Repeat 16 times
@@ -14,15 +14,35 @@ window.onload = function create_grid(){
 
     //Set id to the div
     new_div.setAttribute('id', 'lala')
+    new_div.setAttribute("class", "twojstary")
 
     //Add the text for testing
-    new_div.innerText = o;
+    //new_div.innerText = o;
 
     //Append new div to main div element
     main_div.appendChild(new_div);
-    }
+    }}}
+
+
+const parent = document.querySelector("#main");
+parent.addEventListener('mouseover', event => {
+    if(event.target.className === 'twojstary'){
+        event.target.style.backgroundColor = "black";
+    }})
+
+
+
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', () => {
+    reset_grid();
+})
+
+
+
+function reset_grid(){
+    let list_of_divs = document.querySelectorAll('#lala')
+    for (let i = 0; i < list_of_divs.length; i++){
+    list_of_divs[i].style.backgroundColor = "white";
 }
 }
-
-
-
